be878545dd7e9e9afbdd5d85c1c2d78a0a82736c
snehal-sharma <snehal931994@gmail.com>
2026-01-11 17:38:38 +0530

    Update SieveOfEratosthenes to use n = 50

--- Basic/SieveOfEratosthenes.go
+++ Basic/SieveOfEratosthenes.go
@@ -1,27 +1,22 @@
-
 package main
 
 import "fmt"
 
 func main() {
-	n := 35
+	n := 50
 	arr := make([]int, n+1)
 	var primes []int
 	arr[0] = 1
 	arr[1] = 1
+
 	for i := 2; i <= n; i++ {
 		if arr[i] == 0 {
+			primes = append(primes, i)
 			for j := i + i; j <= n; j = j + i {
 				arr[j] = 1
 			}
 		}
 	}
 
-	for i := 2; i <= n; i++ {
-		if arr[i] == 0 {
-			primes = append(primes, i)
-		}
-	}
-
-	fmt.Println(primes)
+	fmt.Printf("List of Prime numbers under %d : %v", n, primes)
 }
